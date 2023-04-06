2278d96f2c14fae799d9bc03ede3e63d5cf2da5e
S.Ichiki <95273775+SoySoySawada@users.noreply.github.com>
2023-04-06 12:43:49 +0900

    ファイル操作部の単純抽出

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
+use std::error::Error;
 
 /// バイナリプロジェクトの責任の分離
 /// プログラムをmain.rsとlib.rsに分け、ロジックをlib.rsに移動。
@@ -17,24 +19,56 @@ fn main() {
     // collect()にて、それをコレクションに変換
     let args: Vec<String> = env::args().collect();
     
-    let (query, filename) = parse_config(&args);
+    // UPD START Configでの抽出
+    // let (query, filename) = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    // UPD END
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
 
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    run(config);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config{
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
+}
+
+// DEL START 抽出メソッドをConfig構造体のnew関数に移動
+// /// 引数解析器の抽出
+// /// 1番目の引数と2番目の引数を抽出する
+// fn parse_config(args: &[String]) -> (&str, &str) {
+//     let query = &args[1];
+//     let filename = &args[2];
+//     (query, filename)
+// }
+// DEL END
+
+// ADD START run関数の抽出
+/// ファイル操作
+fn run(config: Config) {
     // ファイルのオープンに失敗
-    let mut f = File::open(filename).expect("File open error");
+    let mut f = File::open(config.filename).expect("File open error");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("something went wrong reading the file");
 
     println!("{}", contents);
 }
-
-/// 引数解析器の抽出
-/// 1番目の引数と2番目の引数を抽出する
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
-    (query, filename)
-}
+// ADD END
